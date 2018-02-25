aa82c21d866e048074b93a39460f9a1de814659d
sharukh08 <36474470+sharukh08@users.noreply.github.com>
2018-02-25 23:29:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
-img.onclick = function (game){
+img.onclick = function (){
   img.style.marginleft = '100px';  
     };
     
