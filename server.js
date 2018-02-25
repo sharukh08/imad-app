a17ea045b16b7caeef1d7c547c7c18cdafc33d17
sharukh08 <36474470+sharukh08@users.noreply.github.com>
2018-02-25 17:16:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/s1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 's1.jpg'));
 });
 
