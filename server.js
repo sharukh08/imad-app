1b13186b74f34c41e82b0177cf88f3c13578877d
sharukh08 <36474470+sharukh08@users.noreply.github.com>
2018-02-25 23:00:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,10 +75,10 @@ app.get('/:articleName', function(req, res){
      res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/s1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 's1.jpg'));
