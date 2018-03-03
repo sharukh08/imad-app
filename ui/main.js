9d256463fe96163d907a5b0cb0690494ece3455f
sharukh08 <36474470+sharukh08@users.noreply.github.com>
2018-03-04 01:28:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,16 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-} 
-img.onClick = function (){
-    var interval = setInterval(moveRight, 100);
-  };
-    
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
     
 
+    
+    
+    
+    
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
 
 
