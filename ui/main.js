669fb942c5742646b0ec8fe1ce07c3b332d8961c
sharukh08 <36474470+sharukh08@users.noreply.github.com>
2018-03-10 18:16:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,22 @@
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick = function(){
     
 
     var request = new XMLHttpRequest();
      
      request.onreadystatechange = function() {
-       if(request.readyState == XMLHttpRequest.Done)  
+       if(request.readyState == XMLHttpRequest.Done)  {
      
          if(request.status == 200){
              var counter = request.responseText;
-         }
-         
-     };
-    
-    counter = counter+1;
-    var span = document.getElementById('count');
+             var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+         }
+       }
+     };
+    request.open('GET', 'http://humaidkid.imad.hasura.io/counter', true);
+    request.send(null);
     
 };
 
